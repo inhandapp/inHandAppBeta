5c2943532197dcece8c9a5444d43da0ebf4e6d7a
Hadri_000 <adcaymo@gmail.com>
2015-11-25 10:31:45 -0800

    Transferred source code from Handshake class to SearchActivity class.

--- app/src/main/java/com/inhand/inhandappbeta/SearchActivity.java
+++ app/src/main/java/com/inhand/inhandappbeta/SearchActivity.java
@@ -2,6 +2,7 @@ package com.inhand.inhandappbeta;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
@@ -12,6 +13,11 @@ import android.widget.TextView;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView.OnEditorActionListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 
 public class SearchActivity extends AppCompatActivity implements OnEditorActionListener {
@@ -24,6 +30,9 @@ public class SearchActivity extends AppCompatActivity implements OnEditorActionL
 
     //Define the SharedPreferences object
     private SharedPreferences savedValues;
+
+    //Logging purposes
+    private static final String TAG = "inHandAppBeta";
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -105,4 +114,26 @@ public class SearchActivity extends AppCompatActivity implements OnEditorActionL
 
 /***************** END USER STRING LISTENER & OPERATION METHODS ********************/
 
+/****************** START USER STRING LISTENER & OPERATION METHODS************/
+
+    public void readURL(String keywords) {
+        try {
+            URL oracle = new URL("http://svcs.ebay.com/services/search/FindingService/v1" +
+                "?OPERATION-NAME=findItemsByKeywords" +
+                "&SERVICE-VERSION=1.0.0" +
+                "&SECURITY-APPNAME=inHanda34-8e86-4e05-9e5b-1fdeb7f3cab" +
+                "&RESPONSE-DATA-FORMAT=XML" +
+                "&REST-PAYLOAD" +
+                "&keywords=harry%20potter%20phoenix");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+       }
+        catch(IOException e) {
+            Log.d("TAG", "SearchActivity");
+        }
+    }
 }
