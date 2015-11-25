aec9c7c8d7fe900068cf96cef4e59184d634b735
Hadri_000 <adcaymo@gmail.com>
2015-11-25 09:17:03 -0800

    Handshake source code

--- app/src/main/java/com/inhand/inhandappbeta/Handshake.java
+++ app/src/main/java/com/inhand/inhandappbeta/Handshake.java
@@ -1,8 +1,24 @@
 package com.inhand.inhandappbeta;
 
-/**
- * Created by Hadri_000 on 11/25/2015.
- */
-public class Handshake {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+public class Handshake {
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://svcs.ebay.com/services/search/FindingService/v1" +
+            "?OPERATION-NAME=findItemsByKeywords" +
+            "&SERVICE-VERSION=1.0.0" +
+            "&SECURITY-APPNAME=inHanda34-8e86-4e05-9e5b-1fdeb7f3cab" +
+            "&RESPONSE-DATA-FORMAT=XML" +
+            "&REST-PAYLOAD" +
+            "&keywords=harry%20potter%20phoenix");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
 }
+
