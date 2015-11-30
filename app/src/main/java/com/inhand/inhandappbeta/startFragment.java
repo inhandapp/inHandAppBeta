1c68d1f4fb50e795c076217436cb422ca36ecce2
inHand <inHandapp@gmail.com>
2015-11-30 09:01:47 -0800

    Fixed fragment

---
+++ app/src/main/java/com/inhand/inhandappbeta/startFragment.java
@@ -0,0 +1,24 @@
+package com.inhand.inhandappbeta;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.support.v4.app.Fragment;
+
+public class startFragment extends Fragment {
+    private static TextView topTextView;
+    private static TextView bottomTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Comment
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+
+        return view;
+    }
+
+
+}
