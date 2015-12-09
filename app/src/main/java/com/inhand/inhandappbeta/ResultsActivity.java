ddbc92916ac5beb8331ffd5016cfea23486610dd
inHand <inHandapp@gmail.com>
2015-12-09 10:22:39 -0800

    Added java code to include header toolbar on results_activity

--- app/src/main/java/com/inhand/inhandappbeta/ResultsActivity.java
+++ app/src/main/java/com/inhand/inhandappbeta/ResultsActivity.java
@@ -2,12 +2,24 @@ package com.inhand.inhandappbeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private final String TAG = "Problem!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        //include toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.logo_bar);
+        setSupportActionBar(myToolbar);
+        try {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        } catch (NullPointerException e){
+            Log.v(TAG, "NullPointer");
+        }
     }
 }
