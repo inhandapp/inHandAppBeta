5478eadee8eddb0ec1caa6d3fdaaf2753c045fd6
adam hopkins <ach619@hotmail.com>
2015-12-14 10:53:44 -0800

    added database

--- app/src/main/java/com/inhand/inhandappbeta/eBayItem.java
+++ app/src/main/java/com/inhand/inhandappbeta/eBayItem.java
@@ -2,12 +2,19 @@ package com.inhand.inhandappbeta;
 
 public class eBayItem {
 
+    private  int _id;
     private String title = null; // title
     private String price = null; //price
-    //private String image = null; //image url
     private String link = null; //item url
 
     public eBayItem() {
+    }
+
+    public eBayItem(int _id, String title, String price, String link) {
+        this._id = _id;
+        this.title = title;
+        this.price = price;
+        this.link = link;
     }
 
     public eBayItem(String title, String price, String link) {
@@ -15,6 +22,13 @@ public class eBayItem {
         this.price = price;
         //this.image = image;
         this.link = link;
+    }
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
     }
 
     public void setTitle(String title)     {
